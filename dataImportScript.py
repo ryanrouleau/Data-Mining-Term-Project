9db9f172af1de5c054025eeee14ccec4dc7e1e6d
Joe Rickard <joe.s.rickard@gmail.com>
2017-03-07 20:11:22 -0700

    Updates to usability and sample output.

--- dataImportScript.py
+++ dataImportScript.py
@@ -6,16 +6,25 @@
 #input csv fields:
 # ID,Case Number,Date,Block,IUCR,Primary Type,Description,Location Description,Arrest,Domestic,Beat,District,Ward,Community Area,FBI Code,X Coordinate,Y Coordinate,Year,Updated On,Latitude,Longitude,Location
 #Output fields:
-# YEAR++MONTH, PRIMARY TYPE, LOCATION
+# YEAR++MONTH, PRIMARY TYPE, DESCRIPTION, LOCATION
 
 import csv, sys, re
 
 def main():
-	fIn = open(sys.argv[1],'r', newline='')	#object file f, input csv for read. need to iterate manually to manage memory usage. (textfile, byte-oriented datastream)
-	fOut = open(sys.argv[2], 'w')
-	for row in csv.reader(iter(fIn.readline, '')):
-		newRow = row[2][8:10] + row[2][0:2] + ',' + row[5] + ',' + row[21]
-		print(newRow, file=fOut)
+<<<<<<< HEAD
+	if len(sys.argv) == 3:
+		fIn = open(sys.argv[1],'r', newline='')	#object file f, input csv for read. need to iterate manually to manage memory usage. (textfile, byte-oriented datastream)
+		fOut = open(sys.argv[2], 'w')
+		firstRowFlag = True
+		for row in csv.reader(iter(fIn.readline, '')):
+			if not firstRowFlag:
+				#hardcoded column elements. Sry. See list above for indexing/ labels.
+				newRow = row[2][8:10] + row[2][0:2] + ',' + row[5] + ',' + row[6] + ',' + row[21]
+				print(newRow, file=fOut)
+			else:
+				firstRowFlag = False	
+	else:
+		print("please give args <filename> <inFile name> <outFile name>")
 
 if __name__ == "__main__":
 	main();
