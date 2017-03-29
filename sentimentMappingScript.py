c993cc7524b96c35ee903822d343059b263f8ab1
kybu3653 <kybu3653@colorado.edu>
2017-03-29 15:42:34 -0600

    preliminary sentiment mapping.  NEEDS WORK

---
+++ sentimentMappingScript.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+
+#     PRELIMINARY SENTIMENT MAPPING
+#     Requires input of IUCR code file and mapping file (args 1 and 2 respectively) and output file
+#     Outputs file with IUCR codes and mappings combined.
+
+
+import csv,sys,re
+
+def main():
+  if len(sys.argv) == 4:
+    fIn_code = open(sys.argv[1],'r', newline='')
+    fOut = open(sys.argv[3], 'w')
+    firstRowFlag = True
+    for row in csv.reader(iter(fIn_code.readline, '')):  ##read lines in code file
+      if not firstRowFlag:
+        fIn_map = open(sys.argv[2],'r',newline='')
+        for element in csv.reader(iter(fIn_map.readline, '')):
+          print(element[0])
+          if(element[0] == row[1]):
+            print("MATCH")
+            val = element[1]
+        fIn_map.close()
+        newRow = row[0] + ','+val
+        print(newRow, file=fOut)
+      else:
+        firstRowFlag = False  
+  else:
+    print("please give args <filename> <inFile name> <inFile name > <outFile name>")
+
+
+
+if __name__=="__main__":
+  main();
