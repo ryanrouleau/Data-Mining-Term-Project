96f2bec1c76941ca4d71e9eb096c55a0a9bc2126
kybu3653 <kybu3653@colorado.edu>
2017-04-01 20:58:29 -0600

    Verify in Chicago, filter out OTHER OFFENSE crimes

--- dataImportScript.py
+++ dataImportScript.py
@@ -9,26 +9,31 @@
 # YEAR++MONTH, PRIMARY TYPE, DESCRIPTION, LAT, LON
 
 
-###NEED TO DO#####
-# add a check for lat, long so we know we are in Chicago
-# remove all OTHER OFFENSE crimes because I don't want to be too subjective
-
 import csv, sys, re
 
 def main():
-	if len(sys.argv) == 3:
-		fIn = open(sys.argv[1],'r', newline='')	#object file f, input csv for read. need to iterate manually to manage memory usage. (textfile, byte-oriented datastream)
-		fOut = open(sys.argv[2], 'w')
-		firstRowFlag = True
-		for row in csv.reader(iter(fIn.readline, '')):
-			if not firstRowFlag:
-				#hardcoded column elements. Sry. See list above for indexing/ labels.
-				newRow = row[2][8:10] + row[2][0:2] + ',' + row[4] + ',' + row[19] + ',' + row[20]
-				print(newRow, file=fOut)
-			else:
-				firstRowFlag = False	
-	else:
-		print("please give args <filename> <inFile name> <outFile name>")
+  if len(sys.argv) == 3:
+    fIn = open(sys.argv[1],'r', newline='')	#object file f, input csv for read. need to iterate manually to manage memory usage. (textfile, byte-oriented datastream)
+    fOut = open(sys.argv[2], 'w')
+    firstRowFlag = True
+    for row in csv.reader(iter(fIn.readline, '')):
+      if not firstRowFlag:
+        if(row[5] != "OTHER OFFENSE"):   ##filter out other offense
+          lat = row[19]
+          lon = row[20]
+          try:
+            x=abs(float(lat)-41.8) 
+            y=abs(float(lon)+87.7)
+          except ValueError:
+            x=1
+            y=1
+          if x+y < 1: ##verify in chicago
+            newRow = row[2][8:10] + row[2][0:2] + ',' + row[4] + ',' + lat + ',' + lon
+            print(newRow, file=fOut)
+      else:
+        firstRowFlag = False	
+  else:
+    print("please give args <filename> <inFile name> <outFile name>")
 
 if __name__ == "__main__":
-	main();
+  main();
