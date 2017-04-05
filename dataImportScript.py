e6a9075a07d45842f42eef4d5f3c51b0a3958186
Ryan Rouleau <ryan.c.rouleau@live.com>
2017-04-04 21:03:50 -0600

    Truncate lat,long to 2 dec places & add verbose option to import script

--- dataImportScript.py
+++ dataImportScript.py
@@ -1,5 +1,6 @@
 #
 #	Requires 2 command line arguments; source file name, destination file name.
+#   Optional (3rd) command line arg: 'verbose' to print lines as being written to file
 #	Will take in chicago crime dataset and produce scrubbed csv output for our analysis.
 #
 
@@ -12,7 +13,7 @@
 import csv, sys, re
 
 def main():
-  if len(sys.argv) == 3:
+  if len(sys.argv) == 3 or len(sys.argv) == 4:
     fIn = open(sys.argv[1],'r', newline='')	#object file f, input csv for read. need to iterate manually to manage memory usage. (textfile, byte-oriented datastream)
     fOut = open(sys.argv[2], 'w')
     firstRowFlag = True
@@ -22,7 +23,7 @@ def main():
           lat = row[19]
           lon = row[20]
           try:
-            x=abs(float(lat)-41.8) 
+            x=abs(float(lat)-41.8)
             y=abs(float(lon)+87.7)
           except ValueError:
             x=1
@@ -34,14 +35,17 @@ def main():
               description = "SCHOOL PUBLIC GROUNDS"
             elif(description=="SCHOOL, PRIVATE, GROUNDS" or description == "SCHOOL, PRIVATE, BUILDING"):
               description = "SCHOOL PRIVATE GROUNDS"
-            newRow = row[2][0:2] + ','+row[17]+',' + row[4] + ',' + lat + ',' + lon + ',' +description
+
+            newRow = row[2][0:2] + ','+row[17]+',' + row[4] + ',' + str(round(float(lat),2)) + ',' + str(round(float(lon),2)) + ',' +description
+            if len(sys.argv) == 4 and sys.argv[3] == "verbose":
+                print(newRow)
             print(newRow, file=fOut)
       else:
           newRow = "Month,Year,IUCR,Lat,Lon,Description"
           print(newRow,file=fOut)
-          firstRowFlag = False	
+          firstRowFlag = False
   else:
-    print("please give args <filename> <inFile name> <outFile name>")
+    print("please give args <filename> <inFile name> <outFile name> <(optional)'verbose")
 
 if __name__ == "__main__":
   main();
