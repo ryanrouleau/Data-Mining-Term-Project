ce15bc88198e84a1e28d88e17bf0b32039726ba1
Ryan Rouleau <ryan.c.rouleau@live.com>
2017-04-06 15:09:40 -0600

    cleaned up and added comments for kylee

--- mergeSortAverage.py
+++ mergeSortAverage.py
@@ -22,7 +22,8 @@ def main():
     with open(sys.argv[2], 'r') as fIn:
         fIn_matrix = list(csv.reader(fIn))
 
-    # creating dictionary containing IUCR codes and severity
+    # creating dictionary containing IUCR codes and severity.
+    # the key is IUCR, value is corresponding severity
     map_dict = {}
     with open(sys.argv[1], 'r') as fIn_map:
         map_matrix = list(csv.reader(fIn_map))
@@ -31,40 +32,98 @@ def main():
 
     # storing and removing header column names from matrix so we don't sort them
     col_headers = fIn_matrix[0:1][0]
-    col_headers.append("AvgSeverity")
-    fIn_matrix = fIn_matrix[1:]
+    col_headers.append("AvgSeverity") # appending column header for average severities
+    fIn_matrix = fIn_matrix[1:] # removing column headers from array
 
     print("Merging files & casting year,month,lat,long to float...")
     for row in fIn_matrix:
+        # by default, the merged severity is -1
+        # then if there is a matching severity for the IUCR code of the row, we overwrite that severity
         severity = -1
+        # row[2] is IUCR code in input file
         if row[2] in map_dict:
             severity = map_dict[row[2]]
+        # appending severity to fIn_matrix
         row.append(severity)
-        row[0] = int(row[0])
-        row[1] = int(row[1])
-        row[3] = float(row[3])
-        row[4] = float(row[4])
+        # casting from strings to numerical vals for sorting and average computation
+        row[0] = int(row[0]) # month
+        row[1] = int(row[1]) # year
+        row[3] = float(row[3]) # lat
+        row[4] = float(row[4]) # long
+        row[5] = float(row[5]) # severity
 
     print("Sorting...")
+    # e.g. to sort by 2nd col then 3rd then 1st: op.itemgetter(1,2,0)
     fIn_matrix.sort(key = op.itemgetter(1,0,3,4)) # sort by year, then month, then lat, then long
 
     print("Averaging & writing lat/long blocks to disk...")
     fOut = open(sys.argv[3], 'w')
     col_headers = col_headers[0:2] + col_headers[3:] # removing  IUCR col headers
+    # writing column headers to ouput file
+    # csv.writer.writerow(i) converts array to csv format and writes to file
     csv.writer(fOut).writerow(col_headers)
-    currSectionAvg = [0.0,0]
+
+    """
+    Ok so everything below here is calculating the severity averages for blocks
+    of same month, year, lat, and long and writing to output file.  It's a bit
+    weird but it's the most efficient way of doing it -> it loops through entire
+    file only once and uses O(1) space.
+
+    CALL ME IF YOU'RE CONFUSED
+
+    Vocab:
+        block = consecutive rows with the same month,year,lat,long
+
+    General Idea:
+        We loop through each row in sorted fIn_matrix summing the severities for
+        the current block.  When we hit a new block, we calculate the avg severity
+        from the stored sum and number of rows in the previous block, create an array w/
+        this avg severity and corresponding month,year,lat,long from the previous row
+        for the block and write this row to the output file.
+
+    Determining when we are in a new block when looping through rows in input file:
+        The idea is that we have two arrays (prevRowVals and currRowVals):
+            - prevRowVals holds the month,year,lat,long for the previous row in the loop
+            - currRowVals holds the month,year,lat,long for the current row in the loop
+
+        if prevRowVals does not have the same values as currRowVals, then the current
+        row starts a new block.  The previous row was then the last row in the last block
+
+    When prevRowVals == currRowVals:
+        We add the current row's severity to blockSeveritySum
+        We increment blockSeverityNum by 1
+
+    When prevRowVals != currRowVals:
+        We calculate the average severity from the sum of severities and number of
+        rows in the block that just ended. (i.e. blockSeveritySum/blockSeverityNum)
+        We append this to the prevRowVals array so now we have an array of form:
+            newRow = [month, year, lat, long, avg severity for corresponding block]
+
+    """
+
+    blockSeveritySum = 0.0 # sum of severities for block
+    blockSeverityNum = 0 # number of severities added for each block
+
+    # setting prevRowVals to be first row initially
     prevRowVals = [fIn_matrix[0][0], fIn_matrix[0][1], fIn_matrix[0][3], fIn_matrix[0][4]]
+
     for row in fIn_matrix:
         currRowVals = [row[0], row[1], row[3], row[4]]
         if row[5] != -1: # if there was a matching severity to ICUR in mapping file, otherwise we ignore it
-            if prevRowVals == currRowVals:
-                currSectionAvg[0] += float(row[5])
-                currSectionAvg[1] += 1
-            else: # new section
-                prevRowVals.append(int(currSectionAvg[0]/currSectionAvg[1]))
-                csv.writer(fOut).writerow(prevRowVals)
-                currSectionAvg[0] = float(row[5])
-                currSectionAvg[1] = 1
+            if prevRowVals == currRowVals: # we're in same block as previous iteration
+                blockSeveritySum += row[5] # adding severity
+                blockSeverityNum += 1 # incrementing number of severities added
+            else: # we've hit a new block
+                # calculating average severity for block just finished
+                # newRow = [month, year, lat, long, avg severity for corresponding block]
+                newRow = prevRowVals + [int(blockSeveritySum/blockSeverityNum)]
+                # writing that row to output file
+                csv.writer(fOut).writerow(newRow)
+
+                # resetting blockSeveritySum/Num for new block
+                blockSeveritySum = float(row[5])
+                blockSeverityNum = 1
+                # updating prevRowVals to be currRowVals for new block
                 prevRowVals = currRowVals
 
     fOut.close()
