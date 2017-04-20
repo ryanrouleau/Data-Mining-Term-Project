c82b43ac85cb076b62384ab47b1482589c3fdb9a
kybu3653 <kybu3653@colorado.edu>
2017-04-19 18:25:02 -0600

    Final data import script

--- dataImportScriptFull.py
+++ dataImportScriptFull.py
@@ -18,8 +18,8 @@ import csv, sys, re
 def main():
   if len(sys.argv) == 2 or len(sys.argv) == 3:
     fIn = open(sys.argv[1],'r', newline='')	#object file f, input csv for read. need to iterate manually to manage memory usage. (textfile, byte-oriented datastream)
-    fOut_a = open("./data/ArrestCrimes.csv", 'w')
-    fOut_na = open("./data/NoArrestCrimes.csv",'w')
+    #fOut_a = open("./data/ArrestCrimes.csv", 'w')
+    #fOut_na = open("./data/NoArrestCrimes.csv",'w')
     fOut = open(sys.argv[2],'w')
     firstRowFlag = True
     for row in csv.reader(iter(fIn.readline, '')):
@@ -40,20 +40,22 @@ def main():
               description = "SCHOOL PUBLIC GROUNDS"
             elif(description=="SCHOOL, PRIVATE, GROUNDS" or description == "SCHOOL, PRIVATE, BUILDING"):
               description = "SCHOOL PRIVATE GROUNDS"
-            newRow = row[2][0:2] + ','+row[17]+',' + row[4] + ',' + str(round(float(lat),3)) + ',' + str(round(float(lon),3)) + ',' + description
+            newRow = row[2][0:2] + ','+row[17]+',' + row[4] + ',' + str(round(float(lat),3)) + ',' + str(round(float(lon),3))
             if(row[8]=="true"):  ##SEPARATE BY ARREST/NO ARREST
-              print(newRow,file=fOut_a)
+            #  print(newRow,file=fOut_a)
               arrest="1"
             else:
               arrest ="0"
-              print(newRow,file=fOut_na)
+            if(row[13]=="NA"):
+              print("NA")
+            #  print(newRow,file=fOut_na)
             newRow = newRow +  ',' + arrest
             print(newRow,file=fOut) ## WHOLE DF
       else:
           print(row[8])
-          newRow = "Month,Year,IUCR,Lat,Lon,Description"
-          print(newRow,file=fOut_a)
-          print(newRow,file=fOut_na)
+          newRow = "Month,Year,IUCR,Lat,Lon"
+          #print(newRow,file=fOut_a)
+          #print(newRow,file=fOut_na)
           newRow = newRow + ",Arrest"
           print(newRow,file=fOut)
           firstRowFlag = False
